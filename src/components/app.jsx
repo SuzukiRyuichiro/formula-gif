29b32c1bf11129c709d90929fadc4a872c40bac7
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-03-23 23:22:19 +0900

    managed to put george on it

---
+++ src/components/app.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+import Gif from './gif.jsx'
+import GifList from './gif_list.jsx'
+import SearchBar from './search_bar.jsx'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar />
+          <div className="selected-gif">
+            <Gif />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/gif.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Gif extends Component{
+  render () {
+    return (
+      <img src="https://media.giphy.com/media/QtumEYtcLWmxeBAHxt/giphy.gif" alt="GR63" className="gif" />
+    )
+  }
+}
+
+export default Gif;
