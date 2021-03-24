f453ef435c1a3546ccfddac62f3a029745379c58
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-03-24 14:30:41 +0900

    now copies the giphy link

--- src/components/copy_button.jsx
+++ src/components/copy_button.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
 class CopyButton extends Component{
+  copyGif = () => {
+    navigator.clipboard.writeText(`https://media.giphy.com/media/${this.props.id}/giphy.gif`);
+  }
+
   render () {
     return (
-      <button type="button" class="btn btn-danger copy-button">Danger</button>
+      <button type="button" className="btn btn-danger copy-button" onClick={this.copyGif}>Copy GIF link</button>
     )
   }
 }
